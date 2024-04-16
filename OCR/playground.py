149a8e3733f4829a91b9e045f0d7d34b333bb348
VishalRaj0 <108023331+VishalRaj0@users.noreply.github.com>
2024-04-17 04:25:44 +0530

    Add files via upload

--- OCR/playground.py
+++ OCR/playground.py
@@ -3,19 +3,15 @@ from pytesseract import Output
 import matplotlib.pyplot as plt
 from PIL import Image
 import cv2
+import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract'
-img = cv2.imread("images/Sample1.png")
-img = cv2.resize(img, (600, 360))
-hImg, wImg, _ = img.shape
+img = cv2.imread("images/img.png")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-boxes = pytesseract.image_to_boxes(img)
-for b in boxes.splitlines():
-    b = b.split(' ')
-    print(b)
-    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-    cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (50, 50, 255), 1)
-    cv2.putText(img, b[0], (x, hImg - y + 13), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (50, 205, 50), 1)
-
-cv2.imshow('Detected text', img)
+ret, th1 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV)
+plt.subplot(2, 2, 1)
+plt.title("OTSU+BINARY INVERSION")
+plt.imshow(th1)
+plt.show()
 cv2.waitKey(0)
